c3be8466a805854a3498a3e631556d70ae0f2367
Bulygin Igor Dmitrievich <i.bulygin@tcsbank.ru>
2019-11-25 20:29:48 +0300

    fix

--- gulpfile.js
+++ gulpfile.js
@@ -15,14 +15,14 @@ const jsOrder = require('./src/scripts/order.js');
 let isProd = false;
 
 function htmlTask() {
-    return src(['index.pug', 'editing.pug'].map(s => join('src/html', s)))
+    return src(['index.pug', 'editing.pug', 'popap-delete.pug', 'popap-problem.pug'].map(s => join('src/html', s)))
         .pipe(pug())
         .pipe(rename(path => path.dirname = ''))
         .pipe(dest('dist'));
 }
 
 function cssTask() {
-    return src(['index.less', 'editing.less'].map(s => join('src/styles', s)))
+    return src(['index.less', 'editing.less', 'popap-delete.less', 'popap-problem.less'].map(s => join('src/styles', s)))
         .pipe(less())
         .pipe(gulpif(() => isProd, minifyCSS()))
         .pipe(rename(path => path.dirname = ''))
