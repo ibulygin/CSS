665c5ce17c31a59c5b37c0bf6fac1140ae150ba5
ibulygin <bit19941@gmail.com>
2020-02-03 18:59:08 +0300

    add render

--- src/scripts/init.js
+++ src/scripts/init.js
@@ -3,10 +3,12 @@
     let http = new HttpService();
     let View = app.View;
     let hi = new View();
-    let div = document.querySelector(".page-content__wrapper");
-    
+    let div = document.querySelector(".profession__list");
+    let abouProf = document.querySelector(".profession-about__input-wrapper");
     http.getData().then((data) => {
         let child = hi.render('header', data);
+        let input = hi.render('searchInput');
+        abouProf.innerHTML = input;
         div.innerHTML = child;
     });
     div.innerHTML = "подождите..."
