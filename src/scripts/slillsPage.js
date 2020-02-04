233f4c2e65690dcad33af8dc461d56b2957c99d0
ibulygin <bit19941@gmail.com>
2020-02-04 12:30:57 +0300

    add SkillsPsgee Class

--- src/scripts/slillsPage.js
+++ src/scripts/slillsPage.js
@@ -5,30 +5,33 @@
     function Skills() {
         this.input = document.querySelector('.profession-about__input');
         this.value = this.input.value.trim();
+        this.skills = document.querySelector('.profession__list')
     };
 
     Skills.prototype.init = function () {
         let self = this;
         this.input.addEventListener('keyup', function () {
             let http = new HttpService();
-            let rez = [];
+
             http.getData()
                 .then((data) => {
-
                     let rez = data.filter((element) => {
                         let value = this.value.toUpperCase();
                         let name = element.name.toUpperCase();
                         return name.includes(value);
-                    })
-                    console.log(rez);
-                    self.render("header", rez)
-                })
+                    });
+
+                    self.render('header', rez);
+                });
+            this.skills.innerHTML = "погодите..."
         })
     }
 
     Skills.prototype.render = function (template, data) {
         let view = new View();
-        view.render(template, data).call(this)
+        let lists = document.querySelector('.profession__list');
+        let element = view.render(template, data);
+        lists.innerHTML = element;
     }
     app.Skills = Skills;
 })(app);
