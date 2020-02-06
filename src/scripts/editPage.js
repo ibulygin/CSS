4c485e9d5c22729a1c37bfe66fdca07b037102fe
Игорь Булыгин <igorbulygin@MacBook-Pro-Igor.local>
2020-02-06 23:28:18 +0300

    fix

--- src/scripts/editPage.js
+++ src/scripts/editPage.js
@@ -3,26 +3,62 @@
     let View = app.View;
     let http = new HttpService();
     let view = new View();
-    let div = document.querySelector(".page-content__wrapper");
+    let div = document.querySelector('.page-content__wrapper');
 
-    function EditPage(){};
+    function EditPage(){
+        this.id = this.getId();
+        this.skillName = document.querySelector('.edit-skills__input');
+        this.skillType = document.querySelector('.skills-type__radio');
+        this.telephonyQueue = document.querySelectorAll('.telephony-queue__input');
+        //This is select but may be, has to use option
+        this.queueGroups = document.querySelectorAll('.queue-groups__select');
+        this.btnSave = document.querySelector('.button_confirmation');
+        this.btnDelete = document.querySelector('.button_cancel');
+        this.btnCancel = document.querySelector('.button_delete');
+    };
 
     EditPage.prototype.getId = function() {
         let hash = location.hash;
         return hash.replace('#id=', "");
     };
 
-    
+    EditPage.prototype.listen = function() {
+        this.btnSave.addEventListener('keyup', function() {
+            console.log("save");
+        });
+        this.btnDelete.addEventListener('keyup', function() {
+            console.log("del");
+
+        });
+        this.btnCancel.addEventListener('keyup', function() {
+            console.log("cancel");
+
+        });
+    }
+
+    EditPage.prototype.getChengeData = function() {
+        return {
+            "description" : "Коронавирус лег на папирус",
+            "id" : this.id,
+            "name" : this.skillName,
+            //!!!НЕ ЗАБЫТЬ вытащить значения и преобразовать в массив
+            "queueGroups" : this.queueGroups,
+            //!!!НЕ ЗАБЫТЬ вытащить значения и преобразовать в массив
+            "telephonyQueues" : this.telephonyQueue,
+            "type" : this.skillType
+          }
+    };
     
     (function(){
-        let inctance = new EditPage();
-        inctance.getId();
-        http.getElementById(inctance.getId())
+        let editPage = new EditPage();
+        editPage.getId();
+        http.getElementById(editPage.getId())
             .then((data) => {
                 let child = view.render('edit', data);
                 div.innerHTML = child;
                 console.log(data);
+                editPage.listen();
             })
-    })()
+    })();
     app.EditPage = EditPage;
 })(app);
